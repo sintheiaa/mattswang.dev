c389d41cc04f6851527c2451f7a7e15edf9ec22a
Matt W <venomagentv@gmail.com>
2025-02-04 07:55:22 -0700

    Fixed basePath in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "https://github.com/sintheiaa/mattswang.dev", // Replace with your GitHub repo name
+  basePath: "/https://github.com/sintheiaa/mattswang.dev", // Replace with your GitHub repo name
   trailingSlash: true,
 };
 
